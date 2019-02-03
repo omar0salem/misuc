67f98c0a760630f2a7136e8c248a253f609ed6b0
omar0salem <45674270+omar0salem@users.noreply.github.com>
2019-02-03 21:27:59 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+const deve = ['534895027567198238'];
+const adminp = "-";
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!deve.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminp + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminp + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminp + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminp + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/BlueGamer");
+    message.channel.sendMessage(`**Done ${argresult}**`)
+}
+});
+
+client.login('NTI3MjQ5NDAzNDg0OTYyODE2.DzjKxw.7oti52cgPpopqrsEiADsu9yR0Lk');
